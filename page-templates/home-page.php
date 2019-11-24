ac0d10c53c81cc2b09ef65a4caa8a135cc4dfd7d
Nacho <joseignaciorm13@gmail.com>
2019-11-24 21:50:12 +0100

    Consulta y extracción de datos de publicaciones e investigadores

--- page-templates/home-page.php
+++ page-templates/home-page.php
@@ -127,15 +127,13 @@ get_header(); ?>
 	<h1 class="cell research-sec--title"><a href=""><?php _e('Investigación', 'nar-trans'); ?></a></h1>
 	<div class="grid-x grid-margin-x small-up-1 medium-up-2 large-up-3">
 		<?php
-			$publications = NarTrans_Publication()->get_query(['posts_per_page => 3']);
+			$publications = NarTrans_Publication()->get_query(['posts_per_page' => 3]);
 			while ($publications->have_posts()) : $publications->the_post();
 		?>
-
 			<div class="cell grid-x">
 				<?php get_template_part('template-parts/card', 'publication'); ?>
 			</div>
 		<?php endwhile; ?>
-
 	</div>
 
 	<div class="cell small-12 medium-6 medium-offset-3 large-4 large-offset-8">
